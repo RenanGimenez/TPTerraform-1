43f87386857aef5dbd64e10439473e32cd400b4b
Renan Gimenez <renan.gimenez@hotmail.fr>
2019-05-06 17:08:25 +0200

    ajout variables cidr_block et project_name

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -3,3 +3,11 @@ variable "region" {
     type = "string"
   default = "us-east-1"
 }
+
+variable "cidr_block" {
+  default = "172.23.0.0/16"
+}
+
+variable "project_name" {
+    default = "VPCTerraform"
+}
